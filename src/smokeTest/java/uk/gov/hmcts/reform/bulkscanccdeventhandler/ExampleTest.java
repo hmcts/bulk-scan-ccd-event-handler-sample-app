e613ec3c7f13b7b5293ce98455c69bffbd297495
Aliveni Choppa <aliveni.ch@gmail.com>
2019-06-13 15:57:15 +0100

    Add empty test to smoke tests and functional tests to fix the build

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/ExampleTest.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.bulkscanccdeventhandler;
+
+import org.junit.jupiter.api.Test;
+
+class ExampleTest {
+
+    @Test
+    void replace_this_with_actual_tests() {
+        // TODO: this test is there so that a test report can be created for smoke tests
+        // (otherwise the build fails). Remove when actual smoke tests have been written.
+    }
+}
