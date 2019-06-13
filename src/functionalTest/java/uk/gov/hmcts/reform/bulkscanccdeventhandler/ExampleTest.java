2d5eb7b2ffb1c24adbb4fd4c8d2126931fb13830
Aliveni Choppa <aliveni.ch@gmail.com>
2019-06-13 17:01:42 +0100

    Add test for health end point to the smoke test and functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/ExampleTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/ExampleTest.java
@@ -1,12 +1,27 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler;
 
+import com.typesafe.config.ConfigFactory;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-class ExampleTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ExampleTest {
+
+    private static final String TEST_URL = ConfigFactory.load().getString("test-url");
 
     @Test
-    void replace_this_with_actual_tests() {
+    public void replaceThisWithActualTests() {
         // TODO: this test is there so that a test report can be created for functional tests
         // (otherwise the build fails). Remove when actual smoke tests have been written.
+        Response response = RestAssured
+            .given()
+            .relaxedHTTPSValidation()
+            .baseUri(TEST_URL)
+            .get("/health")
+            .andReturn();
+
+        assertThat(response.getStatusCode()).isEqualTo(200);
     }
 }
