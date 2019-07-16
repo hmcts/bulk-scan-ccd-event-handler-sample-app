7651baf3befe0df9f304b15a2dd93b9e2843454e
Aliveni Choppa <aliveni.ch@gmail.com>
2019-07-16 20:41:20 +0100

    Moved all ocr form validations to a single class

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/services/OcrDataValidator.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/services/OcrDataValidator.java
@@ -1,23 +1,125 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler.services;
 
+import org.springframework.stereotype.Component;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.model.FormType;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.model.OcrDataField;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.model.OcrFieldNames;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.model.out.OcrValidationResult;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.model.out.ValidationStatus;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.util.OcrFormValidationHelper;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.emptyList;
+import static java.util.Collections.singletonList;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.model.FormType.CONTACT;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.model.FormType.PERSONAL;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.model.OcrFieldNames.CONTACT_NUMBER;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.model.OcrFieldNames.EMAIL;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.model.out.ValidationStatus.ERRORS;
 import static uk.gov.hmcts.reform.bulkscanccdeventhandler.model.out.ValidationStatus.SUCCESS;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.model.out.ValidationStatus.WARNINGS;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.util.OcrFormValidationHelper.getOcrFieldNames;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.util.OcrFormValidationHelper.isValidEmailAddress;
+import static uk.gov.hmcts.reform.bulkscanccdeventhandler.util.OcrFormValidationHelper.isValidPhoneNumber;
 
-public final class OcrDataValidator {
+@Component
+public class OcrDataValidator {
 
-    // prevent instantiation
-    private OcrDataValidator() {
+    public OcrValidationResult validate(FormType formType, List<OcrDataField> ocrData) {
+        List<String> errors = validateMandatoryFields(formType, ocrData);
+        List<String> warnings = validateOptionalFields(formType, ocrData);
+
+        return new OcrValidationResult(warnings, errors, getValidationStatus(!errors.isEmpty(), !warnings.isEmpty()));
     }
 
-    public static OcrValidationResult validate(FormType formType, List<OcrDataField> ocrDataFields) {
-        //TODO validate OCr data
-        return new OcrValidationResult(emptyList(), emptyList(), SUCCESS);
+    private List<String> validateMandatoryFields(FormType formType, List<OcrDataField> ocrData) {
+        List<String> mandatoryFields = getMandatoryFieldsForForm(formType);
+        List<String> missingFields = OcrFormValidationHelper.findBlankFields(mandatoryFields, ocrData);
+        List<String> errors = getErrorMessagesForFields(missingFields);
+
+        if (formType.equals(CONTACT)) {
+            String email = OcrFormValidationHelper.findOcrFormFieldValue(EMAIL, ocrData);
+            String phone = OcrFormValidationHelper.findOcrFormFieldValue(CONTACT_NUMBER, ocrData);
+
+            if (!errors.contains(EMAIL) && !isValidEmailAddress(email)) {
+                errors.add("Invalid email address");
+            }
+            if (!errors.contains(CONTACT_NUMBER) && !isValidPhoneNumber(phone)) {
+                errors.add("Invalid phone number");
+            }
+        }
+
+        return errors;
+    }
+
+    private List<String> validateOptionalFields(FormType formType, List<OcrDataField> ocrData) {
+        List<String> optionalFields = getOptionalFieldsForForm(formType);
+        List<String> ocrInputFields = getOcrFieldNames(ocrData);
+
+        List<String> missingFields = findMissingFields(optionalFields, ocrInputFields);
+        return getErrorMessagesForFields(missingFields);
+    }
+
+    private List<String> getErrorMessagesForFields(List<String> missingFields) {
+        return missingFields
+            .stream()
+            .map(field -> String.format("%s is empty", field))
+            .collect(Collectors.toList());
+    }
+
+    private List<String> findMissingFields(List<String> optionalFields, List<String> ocrInputFields) {
+        return optionalFields.stream()
+            .filter(item -> !ocrInputFields.contains(item))
+            .collect(Collectors.toList());
+    }
+
+    private List<String> getMandatoryFieldsForForm(FormType formType) {
+        if (formType.equals(CONTACT)) {
+            return asList(
+                OcrFieldNames.ADDRESS_LINE_1,
+                EMAIL,
+                OcrFieldNames.POST_CODE,
+                OcrFieldNames.COUNTRY,
+                CONTACT_NUMBER
+            );
+
+        } else if (formType.equals(PERSONAL)) {
+            return Arrays.asList(
+                OcrFieldNames.FIRST_NAME,
+                OcrFieldNames.LAST_NAME
+            );
+        }
+        return emptyList();
+    }
+
+    private List<String> getOptionalFieldsForForm(FormType formType) {
+        if (formType.equals(CONTACT)) {
+            return asList(
+                OcrFieldNames.ADDRESS_LINE_1,
+                EMAIL,
+                OcrFieldNames.POST_CODE,
+                OcrFieldNames.COUNTRY,
+                CONTACT_NUMBER
+            );
+        } else if (formType.equals(PERSONAL)) {
+            return singletonList(OcrFieldNames.DATE_OF_BIRTH);
+        }
+        return emptyList();
+    }
+
+    private ValidationStatus getValidationStatus(boolean errorsExist, boolean warningsExist) {
+        if (errorsExist) {
+            return ERRORS;
+        }
+
+        if (warningsExist) {
+            return WARNINGS;
+        }
+        return SUCCESS;
     }
 
 }
--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/util/OcrFormValidationHelper.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/util/OcrFormValidationHelper.java
@@ -1,13 +1,25 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler.util;
 
+import org.apache.commons.validator.routines.EmailValidator;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.model.OcrDataField;
 
 import java.util.List;
+import java.util.regex.Pattern;
+
+import static java.util.stream.Collectors.toList;
+import static org.apache.commons.lang.StringUtils.isBlank;
 
 public final class OcrFormValidationHelper {
 
     private OcrFormValidationHelper() {
         // util class
+    }
+
+    public static List<String> findBlankFields(List<String> fieldNames, List<OcrDataField> ocrData) {
+        return fieldNames
+            .stream()
+            .filter(field -> isBlank(OcrFormValidationHelper.findOcrFormFieldValue(field, ocrData)))
+            .collect(toList());
     }
 
     public static String findOcrFormFieldValue(String fieldName, List<OcrDataField> ocrData) {
@@ -17,5 +29,20 @@ public final class OcrFormValidationHelper {
             .findFirst()
             .map(v -> v.value)
             .orElse(null);
+    }
+
+    public static boolean isValidEmailAddress(String email) {
+        return EmailValidator.getInstance().isValid(email);
+    }
+
+    public static boolean isValidPhoneNumber(String phone) {
+        return Pattern.compile("\\d{10}").matcher(phone).matches();
+    }
+
+    public static List<String> getOcrFieldNames(List<OcrDataField> ocrData) {
+        return ocrData
+            .stream()
+            .map(field -> field.key)
+            .collect(toList());
     }
 }
