3d2da0b86265e763542445e49420eb8a95e379a7
Aliveni Choppa <aliveni.ch@gmail.com>
2019-07-22 11:36:12 +0100

    Added the missing bean config for AuthTokenValidator. Added missing property in application yaml

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/config/AuthConfig.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/config/AuthConfig.java
@@ -1,11 +1,14 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.authorisation.ServiceAuthorisationApi;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
+import uk.gov.hmcts.reform.authorisation.validators.AuthTokenValidator;
+import uk.gov.hmcts.reform.authorisation.validators.ServiceAuthTokenValidator;
 
 @Configuration
 public class AuthConfig {
@@ -16,5 +19,11 @@ public class AuthConfig {
         ServiceAuthorisationApi serviceAuthorisationApi
     ) {
         return AuthTokenGeneratorFactory.createDefaultGenerator(secret, name, serviceAuthorisationApi);
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "idam.s2s-auth.url")
+    public AuthTokenValidator tokenValidator(ServiceAuthorisationApi s2sApi) {
+        return new ServiceAuthTokenValidator(s2sApi);
     }
 }
