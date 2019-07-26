4474221f2cbedd9fcd1c041ced6f8f998eaf4b3e
Aliveni Choppa <aliveni.ch@gmail.com>
2019-07-26 12:32:18 +0100

    Add s2s config in terraform files for functional tests

---
+++ infrastructure/test_config.tf
@@ -0,0 +1,17 @@
+
+data "azurerm_key_vault_secret" "source_test_s2s_secret" {
+  key_vault_id = "${data.azurerm_key_vault.s2s_key_vault.id}"
+  name         = "microservicekey-bulk-scan-sample-app-tests"
+}
+
+resource "azurerm_key_vault_secret" "test_s2s_secret" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "test-s2s-secret"
+  value        = "${data.azurerm_key_vault_secret.source_test_s2s_secret.value}"
+}
+
+resource "azurerm_key_vault_secret" "s2s_secret" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "s2s-secret"
+  value        = "${data.azurerm_key_vault_secret.s2s_secret.value}"
+}
