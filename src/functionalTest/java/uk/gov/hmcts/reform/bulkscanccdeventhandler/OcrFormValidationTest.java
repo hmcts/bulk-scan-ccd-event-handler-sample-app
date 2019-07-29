845d00b945a3a27ca56aefbcac0ccf63a0a8d987
Aliveni Choppa <aliveni.ch@gmail.com>
2019-07-29 11:34:18 +0100

    load s2s properties from config

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/OcrFormValidationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/OcrFormValidationTest.java
@@ -1,37 +1,49 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import io.restassured.RestAssured;
 import io.restassured.mapper.ObjectMapperType;
 import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.model.out.OcrValidationResponse;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.model.out.ValidationStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
 
+@ExtendWith(SpringExtension.class)
 @TestPropertySource("classpath:application.conf")
 class OcrFormValidationTest {
 
-    @Value("${test-url}")
     private String testUrl;
 
-    @Value("${s2s-url}")
     private String s2sUrl;
 
-    @Value("${s2s-name}")
     private String s2sName;
 
-    @Value("${s2s-secret}")
     private String s2sSecret;
+
+    private Config config;
 
     private TestHelper testHelper = new TestHelper();
 
+    @BeforeAll
+    public void setUp() {
+        this.config = ConfigFactory.load();
+        this.testUrl = config.getString("test-url");
+        this.s2sUrl = config.getString("test-s2s-url");
+        this.s2sName = config.getString("test-s2s-name");
+        this.s2sSecret = config.getString("test-s2s-secret");
+    }
+
     @Test
-    void should_validate_ocr_data_and_return_success() {
+    void should_validate_ocr_data_and_return_success() throws Exception {
         Response response = RestAssured
             .given()
             .relaxedHTTPSValidation()
