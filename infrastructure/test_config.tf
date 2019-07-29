e3d1c49cf097e4e87cf39477151ae48e9ffa93aa
Aliveni Choppa <aliveni.ch@gmail.com>
2019-07-29 15:15:50 +0100

    Change the s2s secret name for tests

--- infrastructure/test_config.tf
+++ infrastructure/test_config.tf
@@ -6,6 +6,6 @@ data "azurerm_key_vault_secret" "source_test_s2s_secret" {
 
 resource "azurerm_key_vault_secret" "test_s2s_secret" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "test-s2s-secret"
+  name         = "test-sample-app-s2s-secret"
   value        = "${data.azurerm_key_vault_secret.source_test_s2s_secret.value}"
 }
