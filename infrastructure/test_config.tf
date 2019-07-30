558867d51f63025efd947004fa67a099586f3633
Aliveni Choppa <aliveni.ch@gmail.com>
2019-07-30 10:41:19 +0100

    rename the keyvault secret name for the s2s secret used in tests

--- infrastructure/test_config.tf
+++ infrastructure/test_config.tf
@@ -4,8 +4,8 @@ data "azurerm_key_vault_secret" "source_test_s2s_secret" {
   name         = "microservicekey-bulk-scan-sample-app-tests"
 }
 
-resource "azurerm_key_vault_secret" "bulk_scan_sample_app_tests_s2s_secret" {
+resource "azurerm_key_vault_secret" "sample_app_tests_s2s_secret" {
   key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
-  name         = "bulk-scan-sample-app-tests-s2s-secret"
+  name         = "sample-app-tests-s2s-secret"
   value        = "${data.azurerm_key_vault_secret.source_test_s2s_secret.value}"
 }
