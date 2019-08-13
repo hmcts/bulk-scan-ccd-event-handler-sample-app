e69e2855a07de2e5cf22ed7dad7e018dd2f0a892
Rafal Kondratowicz <rkondr@gmail.com>
2019-08-13 17:24:15 +0100

    fix sonar warnings

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/transformation/services/InvalidExceptionRecordException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/transformation/services/InvalidExceptionRecordException.java
@@ -4,10 +4,14 @@ import java.util.List;
 
 public class InvalidExceptionRecordException extends RuntimeException {
 
-    public final List<String> errors;
+    private final List<String> errors;
 
     public InvalidExceptionRecordException(List<String> errors) {
         super("Validation errors: " + String.join(", ", errors));
         this.errors = errors;
+    }
+
+    public List<String> getErrors() {
+        return errors;
     }
 }
