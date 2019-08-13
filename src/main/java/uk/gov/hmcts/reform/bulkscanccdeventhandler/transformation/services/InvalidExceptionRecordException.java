c728b703dd97fa1b9046b1321e3f15b76181710b
Rafal Kondratowicz <rkondr@gmail.com>
2019-08-13 16:56:01 +0100

    Store list of errors in ocr validation exception

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/transformation/services/InvalidExceptionRecordException.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/transformation/services/InvalidExceptionRecordException.java
@@ -1,7 +1,13 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler.transformation.services;
 
+import java.util.List;
+
 public class InvalidExceptionRecordException extends RuntimeException {
-    public InvalidExceptionRecordException(String message) {
-        super(message);
+
+    public final List<String> errors;
+
+    public InvalidExceptionRecordException(List<String> errors) {
+        super(String.join(",", errors));
+        this.errors = errors;
     }
 }
