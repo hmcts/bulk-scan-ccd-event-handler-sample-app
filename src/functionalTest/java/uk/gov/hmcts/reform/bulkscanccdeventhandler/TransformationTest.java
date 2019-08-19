83656ea55d0fb372908ea0ea05e50ecac994b442
Donatas Martinkus <doncem@users.noreply.github.com>
2019-08-19 14:16:58 +0100

    Reduce duplicate assertions in functional test

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/TransformationTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/TransformationTest.java
@@ -8,10 +8,10 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.transformation.services.ExceptionRecordToCaseTransformer;
 
-import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
@@ -33,48 +33,21 @@ public class TransformationTest {
 
         JsonPath transformationResponse = response.getBody().jsonPath();
 
-        assertThat(transformationResponse.getList("warnings")).isEmpty();
+        assertSoftly(softly -> {
+            softly.assertThat(transformationResponse.getList("warnings")).isEmpty();
+            softly.assertThat(transformationResponse.getMap("case_creation_details").get("case_type_id"))
+                .isEqualTo(ExceptionRecordToCaseTransformer.CASE_TYPE_ID);
+            softly.assertThat(transformationResponse.getMap("case_creation_details").get("event_id"))
+                .isEqualTo(ExceptionRecordToCaseTransformer.EVENT_ID);
 
-        Map<String, Object> details = transformationResponse.getMap("case_creation_details");
+            Map<String, Object> caseData = (Map<String, Object>) transformationResponse
+                .getMap("case_creation_details")
+                .get("case_data");
 
-        assertThat(details.get("case_type_id")).isEqualTo(ExceptionRecordToCaseTransformer.CASE_TYPE_ID);
-        assertThat(details.get("event_id")).isEqualTo(ExceptionRecordToCaseTransformer.EVENT_ID);
+            softly.assertThat(caseData.get("email")).isEqualTo("non-empty-email");
 
-        Map<String, Object> caseData = (Map<String, Object>) details.get("case_data");
-
-        assertThat(caseData.get("legacyId")).isNull();
-        assertThat(caseData.get("firstName")).isEqualTo("FIRST");
-        assertThat(caseData.get("lastName")).isEqualTo("NAME");
-        assertThat(caseData.get("dateOfBirth")).isNull();
-        assertThat(caseData.get("contactNumber")).isNull();
-        assertThat(caseData.get("email")).isEqualTo("non-empty-email");
-
-        Map<String, Object> address = (Map<String, Object>) caseData.get("address");
-
-        assertThat(address.get("addressLine1")).isNull();
-        assertThat(address.get("addressLine2")).isNull();
-        assertThat(address.get("addressLine3")).isNull();
-        assertThat(address.get("postCode")).isNull();
-        assertThat(address.get("postTown")).isNull();
-        assertThat(address.get("county")).isNull();
-        assertThat(address.get("country")).isNull();
-
-        assertThat(caseData.get("scannedDocuments"))
-            .asList()
-            .hasSize(1);
-
-        Map<String, Object> document = ((List<Map<String, Map<String, Object>>>) caseData.get("scannedDocuments"))
-            .get(0)
-            .get("value");
-
-        assertThat(document.get("type")).isEqualTo("Form");
-        assertThat(document.get("subtype")).isEqualTo("XYZ");
-        assertThat(document.get("url")).isEqualTo("url");
-        assertThat(document.get("controlNumber")).isEqualTo("987654321");
-        assertThat(document.get("fileName")).isEqualTo("987654321-123456789.pdf");
-        assertThat(document.get("scannedDate")).isEqualTo("2019-08-01T00:01:02.345");
-        assertThat(document.get("deliveryDate")).isEqualTo("2019-08-01T01:02:03.456");
-        assertThat(document.get("exceptionRecordReference")).isEqualTo("id");
+            softly.assertAll();
+        });
     }
 
     @Test
@@ -91,8 +64,6 @@ public class TransformationTest {
         assertThat(transformationResponse.getList("warnings"))
             .hasSize(1)
             .containsOnly("'email' is empty");
-
-        // full blow response is checked in happy path scenario above ^
 
         assertThat(transformationResponse.getMap("case_creation_details").get("email")).isNull();
     }
