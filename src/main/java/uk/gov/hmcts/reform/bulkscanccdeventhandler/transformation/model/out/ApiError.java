788c04dc1b9c5751dcbe85749beaf8488aa3fa82
mdayican <mustafa.dayican@hmcts.net>
2019-08-23 10:27:22 +0100

    return error messages in JSON format (#69)

---
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/transformation/model/out/ApiError.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.reform.bulkscanccdeventhandler.transformation.model.out;
+
+public class ApiError {
+    public final String error;
+
+    public ApiError(String error) {
+        this.error = error;
+    }
+
+}
