794a4624e4b8a15f6cf27dc19b60def86296361b
apereverzin <andrey.pereverzin@hotmail.com>
2019-10-09 12:41:04 +0100

    Format

--- src/test/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/transformation/services/CaseValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/transformation/services/CaseValidatorTest.java
@@ -22,8 +22,8 @@ public class CaseValidatorTest {
 
     @Test
     public void should_return_warning_when_email_is_invalid() {
-            assertThat(validator.getWarnings(caseWithEmail("invalidemail")))
-                .containsExactly("'email' is invalid invalidemail");
+        assertThat(validator.getWarnings(caseWithEmail("invalidemail")))
+            .containsExactly("'email' is invalid invalidemail");
     }
 
     @Test
