66c36d481232a6a6fdc7ca29b30ae47631b32d38
Rafal Kondratowicz <rkondr@gmail.com>
2019-11-19 16:54:14 +0000

    test wip

--- src/test/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/caseupdate/controllers/UpdateCaseControllerTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/caseupdate/controllers/UpdateCaseControllerTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.bulkscanccdeventhandler.caseupdate.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -17,14 +18,19 @@ import uk.gov.hmcts.reform.bulkscanccdeventhandler.caseupdate.services.CaseUpdat
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.common.auth.AuthService;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.common.auth.ForbiddenException;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.common.auth.UnauthenticatedException;
+import uk.gov.hmcts.reform.bulkscanccdeventhandler.common.model.out.*;
 
+import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
+import static java.time.format.DateTimeFormatter.ISO_DATE_TIME;
+import static java.util.Arrays.asList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.http.HttpStatus.FORBIDDEN;
 import static org.springframework.http.HttpStatus.UNAUTHORIZED;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SuppressWarnings("checkstyle:lineLength")
@@ -48,6 +54,102 @@ class UpdateCaseControllerTest {
 
         sendRequest("{}")
             .andExpect(status().is(status.value()));
+    }
+
+    @Test
+    public void should_return_expected_case_details() throws Exception {
+        // given
+        SampleCase sampleCase = new SampleCase(
+            "legacy-id",
+            "first-name",
+            "last-name",
+            "date-of-birth",
+            "contact-number",
+            "email",
+            new Address(
+                "address-line-1",
+                "address-line-2",
+                "address-line-3",
+                "post-code",
+                "post-town",
+                "county",
+                "country"
+            ),
+            asList(
+                new Item<>(new ScannedDocument(
+                    "type-1",
+                    "subtype-1",
+                    new DocumentUrl(
+                        "url-1",
+                        "binary-url-1",
+                        "file-name-1"
+                    ),
+                    "dcn-1",
+                    "file-name-1",
+                    LocalDateTime.parse("2011-12-03T10:15:30.123", ISO_DATE_TIME),
+                    LocalDateTime.parse("2011-12-04T10:15:30.123", ISO_DATE_TIME),
+                    "ref-1"
+                )),
+                new Item<>(new ScannedDocument(
+                    "type-2",
+                    "subtype-2",
+                    new DocumentUrl(
+                        "url-2",
+                        "binary-url-2",
+                        "file-name-2"
+                    ),
+                    "dcn-2",
+                    "file-name-2",
+                    LocalDateTime.parse("2011-12-05T10:15:30.123", ISO_DATE_TIME),
+                    LocalDateTime.parse("2011-12-06T10:15:30.123", ISO_DATE_TIME),
+                    "ref-2"
+                ))
+            ),
+            "er-id"
+        );
+        given(caseUpdater.update(any(), any())).willReturn(sampleCase);
+
+        sendRequest("{}")
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.case_update_details.case_type_id").value("case-type-id"))
+            .andExpect(jsonPath("$.case_update_details.event_id").value("event-id"))
+            .andExpect(jsonPath("$.case_update_details.case_data.legacyId").value("legacy-id"))
+            .andExpect(jsonPath("$.case_update_details.case_data.firstName").value("first-name"))
+            .andExpect(jsonPath("$.case_update_details.case_data.lastName").value("last-name"))
+            .andExpect(jsonPath("$.case_update_details.case_data.dateOfBirth").value("date-of-birth"))
+            .andExpect(jsonPath("$.case_update_details.case_data.contactNumber").value("contact-number"))
+            .andExpect(jsonPath("$.case_update_details.case_data.email").value("email"))
+            .andExpect(jsonPath("$.case_update_details.case_data.address.addressLine1").value("address-line-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.address.addressLine2").value("address-line-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.address.addressLine3").value("address-line-3"))
+            .andExpect(jsonPath("$.case_update_details.case_data.address.postCode").value("post-code"))
+            .andExpect(jsonPath("$.case_update_details.case_data.address.postTown").value("post-town"))
+            .andExpect(jsonPath("$.case_update_details.case_data.address.county").value("county"))
+            .andExpect(jsonPath("$.case_update_details.case_data.address.country").value("country"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.type").value("type-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.subtype").value("subtype-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.url.document_url").value("url-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.url.document_binary_url").value("binary-url-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.url.document_filename").value("file-name-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.controlNumber").value("dcn-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.fileName").value("file-name-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.scannedDate").value("2011-12-03T10:15:30.123"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.deliveryDate").value("2011-12-04T10:15:30.123"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[0].value.exceptionRecordReference").value("ref-1"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.type").value("type-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.subtype").value("subtype-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.url.document_url").value("url-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.url.document_binary_url").value("binary-url-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.url.document_filename").value("file-name-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.controlNumber").value("dcn-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.fileName").value("file-name-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.scannedDate").value("2011-12-05T10:15:30.123"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.deliveryDate").value("2011-12-06T10:15:30.123"))
+            .andExpect(jsonPath("$.case_update_details.case_data.scannedDocuments[1].value.exceptionRecordReference").value("ref-2"))
+            .andExpect(jsonPath("$.case_update_details.case_data.bulkScanCaseReference").value("er-id"))
+            .andExpect(jsonPath("$.warnings[0]").value("warning-1"))
+            .andExpect(jsonPath("$.warnings[1]").value("warning-2"));
+
     }
 
     private static Stream<Arguments> exceptionsAndStatuses() {
