09a6686bce105929370f3ddd61e7627cf6715d17
mdayican <mustafa.dayican@hmcts.net>
2020-01-24 14:57:42 +0000

    fix test json data

--- src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/UpdateCaseTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/UpdateCaseTest.java
@@ -2,8 +2,10 @@ package uk.gov.hmcts.reform.bulkscanccdeventhandler;
 
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import uk.gov.hmcts.reform.bulkscanccdeventhandler.caseupdate.services.CaseUpdater;
@@ -18,7 +20,7 @@ public class UpdateCaseTest {
     private final TestHelper testHelper = new TestHelper();
 
     @Test
-    public void should_update_case() {
+    public void should_update_case() throws JSONException {
         // given
         String request = TestHelper.fileContentAsString("updatecase/valid-request.json");
 
@@ -30,5 +32,7 @@ public class UpdateCaseTest {
         JsonPath json = response.getBody().jsonPath();
         assertThat(json.getList("warnings")).isEmpty();
         assertThat(json.getMap("case_update_details").get("event_id")).isEqualTo(CaseUpdater.EVENT_ID);
+        String expectedJson = TestHelper.fileContentAsString("updatecase/valid-response.json");
+        JSONAssert.assertEquals(expectedJson, response.getBody().asString(), false);
     }
 }
