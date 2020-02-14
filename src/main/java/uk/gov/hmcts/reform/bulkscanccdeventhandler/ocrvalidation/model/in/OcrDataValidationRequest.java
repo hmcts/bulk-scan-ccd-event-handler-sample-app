c23019f5b39333b30fab1d2efa1713af4950ef1e
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2020-02-14 11:21:36 +0000

    Upgrade checkstyle (#126)

--- src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/ocrvalidation/model/in/OcrDataValidationRequest.java
+++ src/main/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/ocrvalidation/model/in/OcrDataValidationRequest.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.annotations.ApiModelProperty;
 
 import java.util.List;
-
 import javax.validation.constraints.NotEmpty;
 
 public class OcrDataValidationRequest {
