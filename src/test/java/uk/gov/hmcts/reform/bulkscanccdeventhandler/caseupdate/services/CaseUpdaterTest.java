b7ba90d125a26af6dc914399cb6e6f370b363120
Donatas Martinkus <doncem@users.noreply.github.com>
2020-02-24 18:48:19 +0000

    Cover branches for case updater

--- src/test/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/caseupdate/services/CaseUpdaterTest.java
+++ src/test/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/caseupdate/services/CaseUpdaterTest.java
@@ -218,4 +218,80 @@ public class CaseUpdaterTest {
             .isEqualTo("binary_url_2");
 
     }
+
+    @Test
+    public void should_update_case_data_without_any_documents() {
+        // given
+
+        Address originalCaseAddress = new Address("a", "b", "c", "d", "e", "f", "g");
+        Address exceptionRecordAddress = new Address("0", "1", "2", "3", "4", "5", "6");
+
+        LocalDateTime caseScannedDate = now();
+        LocalDateTime caseDeliveryDate = now();
+
+        SampleCase originalCase = new SampleCase(
+            "legacy-id",
+            "first-name",
+            "last-name",
+            "date-of-birth",
+            "contact-number",
+            "email",
+            originalCaseAddress,
+            emptyList(),
+            "er-id"
+        );
+
+        ExceptionRecord exceptionRecord = new ExceptionRecord(
+            "er-id",
+            "er-case-type",
+            "er-pobox",
+            "er-jurisdiction",
+            "er-form-type",
+            JourneyClassification.SUPPLEMENTARY_EVIDENCE_WITH_OCR,
+            now(),
+            now(),
+            emptyList(),
+            emptyList()
+        );
+
+        given(addressExtractor.extractFrom(any())).willReturn(exceptionRecordAddress);
+
+        // when
+        SuccessfulUpdateResponse result =
+            caseUpdater.update(
+                new CaseUpdate(
+                    exceptionRecord,
+                    new CaseDetails("1234567890","some_type", originalCase)
+                )
+            );
+
+        // then
+        assertThat(result.caseUpdateDetails.caseData.address.addressLine1).isEqualTo(exceptionRecordAddress.addressLine1);
+        assertThat(result.caseUpdateDetails.caseData.address.addressLine2).isEqualTo(exceptionRecordAddress.addressLine2);
+        assertThat(result.caseUpdateDetails.caseData.address.addressLine3).isEqualTo(exceptionRecordAddress.addressLine3);
+        assertThat(result.caseUpdateDetails.caseData.address.country).isEqualTo(exceptionRecordAddress.country);
+        assertThat(result.caseUpdateDetails.caseData.address.county).isEqualTo(exceptionRecordAddress.county);
+        assertThat(result.caseUpdateDetails.caseData.address.postCode).isEqualTo(exceptionRecordAddress.postCode);
+        assertThat(result.caseUpdateDetails.caseData.address.postTown).isEqualTo(exceptionRecordAddress.postTown);
+
+        assertThat(result.caseUpdateDetails.caseData)
+            .extracting(c -> tuple(
+                c.legacyId,
+                c.firstName,
+                c.lastName,
+                c.dateOfBirth,
+                c.bulkScanCaseReference
+            ))
+            .isEqualTo(tuple(
+                originalCase.legacyId,
+                originalCase.firstName,
+                originalCase.lastName,
+                originalCase.dateOfBirth,
+                originalCase.bulkScanCaseReference
+            ));
+
+        assertThat(result.warnings).isEmpty();
+        assertThat(result.caseUpdateDetails.eventId).isEqualTo(CaseUpdater.EVENT_ID);
+        assertThat(result.caseUpdateDetails.caseData.scannedDocuments).isEmpty();
+    }
 }
