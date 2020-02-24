971f9a8bfe1902d3043917e9c68e169d61266485
Donatas Martinkus <doncem@users.noreply.github.com>
2020-02-24 18:03:15 +0000

    Remove deprecated string interpolation

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -3,8 +3,8 @@ provider "azurerm" {
 }
 
 locals {
-  is_preview          = "${(var.env == "preview" || var.env == "spreview")}"
-  local_env           = "${local.is_preview ? "aat" : var.env}"
+  is_preview          = var.env == "preview" || var.env == "spreview"
+  local_env           = local.is_preview ? "aat" : var.env
   s2s_rg              = "rpe-service-auth-provider-${local.local_env}"
   s2s_url             = "http://${local.s2s_rg}.service.core-compute-${local.local_env}.internal"
 
@@ -14,15 +14,15 @@ locals {
 module "bulk-scan-ccd-event-handler-sample-app" {
   source              = "git@github.com:hmcts/cnp-module-webapp?ref=master"
   product             = "${var.product}-${var.component}"
-  location            = "${var.location_app}"
-  env                 = "${var.env}"
-  ilbIp               = "${var.ilbIp}"
-  subscription        = "${var.subscription}"
-  capacity            = "${var.capacity}"
-  common_tags         = "${var.common_tags}"
-  enable_ase          = "${var.enable_ase}"
+  location            = var.location_app
+  env                 = var.env
+  ilbIp               = var.ilbIp
+  subscription        = var.subscription
+  capacity            = var.capacity
+  common_tags         = var.common_tags
+  enable_ase          = var.enable_ase
 
   app_settings = {
-    S2S_URL                 = "${local.s2s_url}"
+    S2S_URL                 = local.s2s_url
   }
 }
--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,8 +1,8 @@
 output "TEST_S2S_URL" {
-  value = "${local.s2s_url}"
+  value = local.s2s_url
 }
 
 output "TEST_S2S_NAME" {
   sensitive = true
-  value     = "${var.test_s2s_name}"
+  value     = var.test_s2s_name
 }
