2b037d6c020cfe1bfeeb85a47445f04f2707edf4
Donatas Martinkus <doncem@users.noreply.github.com>
2020-02-25 14:41:37 +0000

    Fix integration test resource

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/OcrValidationTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/OcrValidationTest.java
@@ -9,7 +9,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.IOException;
@@ -26,7 +25,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ComponentScan(basePackages = "...", lazyInit = true)
 @ContextConfiguration
 @SpringBootTest
-@TestPropertySource("classpath:application.conf")
 class OcrValidationTest {
 
     @Autowired
