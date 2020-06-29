99c3c84ea932779f0181a057cf96d9b4e9ecc789
mdayican <mustafa.dayican@hmcts.net>
2020-06-29 01:04:18 +0100

    test the test

--- src/integrationTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/OcrValidationContractTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/bulkscanccdeventhandler/OcrValidationContractTest.java
@@ -9,14 +9,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import uk.gov.hmcts.reform.bulkscanccdeventhandler.common.auth.AuthService;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @Provider("sample_app_ocr_validation")
@@ -28,14 +23,10 @@ public class OcrValidationContractTest {
     @LocalServerPort
     private int port;
 
-    @MockBean
-    private AuthService authService;
 
     @BeforeEach
     void setupTestTarget(PactVerificationContext context) {
         context.setTarget(new HttpTestTarget("localhost", port, "/"));
-        when(authService.authenticate(any())).thenReturn("bulk_scan_sample_app_test");
-        doNothing().when(authService).assertIsAllowedService(any());
     }
 
     @TestTemplate
